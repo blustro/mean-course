a73271336de8ddf810e7263145d443dfff97b0d3
blustro <brunolustro@gmail.com>
2021-03-05 15:36:29 +0000

    Fetching posts from the server

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("First middleware");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send("Hello from express!");
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "wecvwfw46489",
+      title: "First server-side post",
+      content: "This is coming from the server",
+    },
+    {
+      id: "hrhrge789432",
+      title: "Second server-side post",
+      content: "This is also coming from the server",
+    },
+  ];
+  res.status(200).json({
+    message: "Posts fetch successfully!",
+    posts: posts,
+  });
 });
 
 module.exports = app;

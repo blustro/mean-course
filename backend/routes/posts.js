590232a9447a9d8c07ad18feb660bdd048fc2e26
blustro <brunolustro@gmail.com>
2021-03-13 11:14:42 +0000

    Added router protection for posts

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -3,6 +3,7 @@ const multer = require("multer");
 
 const Post = require("../models/post");
 const router = express.Router();
+const checkAuth = require("../middleware/check-auth");
 
 const MIME_TYPE_MAP = {
   "image/png": "png",
@@ -26,43 +27,53 @@ const storage = multer.diskStorage({
   },
 });
 
-router.post("", multer({ storage }).single("image"), (req, res, next) => {
-  const url = req.protocol + "://" + req.get("host");
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-    imagePath: url + "/images/" + req.file.filename,
-  });
-  post.save().then((createdPost) => {
-    createdPost.toObject();
-    res.status(201).json({
-      message: "Post added successfully!",
-      post: {
-        ...createdPost,
-        id: createdPost._id,
-      },
-    });
-  });
-});
-
-router.put("/:id", multer({ storage }).single("image"), (req, res, next) => {
-  let imagePath = req.body.imagePath;
-  if (req.file) {
+router.post(
+  "",
+  checkAuth,
+  multer({ storage }).single("image"),
+  (req, res, next) => {
     const url = req.protocol + "://" + req.get("host");
-    imagePath = url + "/images/" + req.file.filename;
+    const post = new Post({
+      title: req.body.title,
+      content: req.body.content,
+      imagePath: url + "/images/" + req.file.filename,
+    });
+    post.save().then((createdPost) => {
+      createdPost.toObject();
+      res.status(201).json({
+        message: "Post added successfully!",
+        post: {
+          ...createdPost,
+          id: createdPost._id,
+        },
+      });
+    });
   }
+);
 
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content,
-    imagePath,
-  });
-  console.log(post);
-  Post.updateOne({ _id: req.params.id }, post).then((result) => {
-    res.status(200).json({ message: "Update successful!" });
-  });
-});
+router.put(
+  "/:id",
+  checkAuth,
+  multer({ storage }).single("image"),
+  (req, res, next) => {
+    let imagePath = req.body.imagePath;
+    if (req.file) {
+      const url = req.protocol + "://" + req.get("host");
+      imagePath = url + "/images/" + req.file.filename;
+    }
+
+    const post = new Post({
+      _id: req.body.id,
+      title: req.body.title,
+      content: req.body.content,
+      imagePath,
+    });
+    console.log(post);
+    Post.updateOne({ _id: req.params.id }, post).then((result) => {
+      res.status(200).json({ message: "Update successful!" });
+    });
+  }
+);
 
 router.get("", (req, res, next) => {
   const pageSize = +req.query.pagesize;
@@ -96,7 +107,7 @@ router.get("/:id", (req, res, next) => {
   });
 });
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", checkAuth, (req, res, next) => {
   Post.deleteOne({ _id: req.params.id }).then((result) => {
     console.log(result);
     res.status(200).json({ message: "Post deleted!" });

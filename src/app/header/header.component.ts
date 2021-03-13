834b7189b2a88ea98a3a638e6970c1a9aedfae8b
blustro <brunolustro@gmail.com>
2021-03-13 17:16:51 +0000

    Handle Authentication in the UI

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  userIsAuthenticated = false;
+  private authListenerSubs: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.authListenerSubs = this.authService
+      .getAuthStatusListener()
+      .subscribe((isAuthentcicated) => {
+        this.userIsAuthenticated = isAuthentcicated;
+      });
   }
 
+  ngOnDestroy() {
+    this.authListenerSubs.unsubscribe();
+  }
 }
